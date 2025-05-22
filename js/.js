cf2cd55d7b6bb2eb17bb3ae9c6caa18648089afa
Ashok Bohara <boharaashok32@gmail.com>
2025-05-22 20:52:57 +0545

    modified

--- js/.js
+++ js/.js
@@ -7,8 +7,9 @@ var ashok = {
         console.log("Hello, my name is " + this.name + " and I am " + this.age + " years old.");
     }
 };
+
 var bohara =
- {
+{
     name: "bohara",
     age: 20,
     greet: function()
@@ -16,5 +17,22 @@ var bohara =
         console.log("Hello, my name is " + this.name+ " and I am " + this.age + " years old.");
     }
 };
+
 ashok.greet();
 bohara.greet();
+let a= 10;
+let b= 20;
+function add(a, b)
+{
+return a + b;
+}
+console.log(add(a, b))
+
+
+
+
+
+// hello hello this is a test MessageChannel...
+
+// my name is ashok bohara and this is a test message to check if my wakatime is working or Notification,
+ //api key seems to be broken, this is not so CSSMatrixComponent, the subtle error is just the methods
