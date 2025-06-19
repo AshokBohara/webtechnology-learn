48c3265a55f909d85cc2f34b051751b7b5c26caf
Ashok Bohara <boharaashok32@gmail.com>
2025-06-19 09:02:05 +0545

    learning js

---
+++ js/index2.js
@@ -0,0 +1,38 @@
+console.log('hello world')
+let Name ='Ashok';
+console.log(Name);
+
+//object in javascript
+let person = {
+name: 'Ashok',
+age:20
+};
+console.log(person);
+
+//Array
+let selectedcolor=['red','blue'];
+console.log(selectedcolor[0]);//here in array 0 assign the first value in array and 1 assign the second value like 0 assign  "red" and 1 assign "blue".
+//function
+function Agreet() {
+    console.log('hello world');
+}
+Agreet();
+
+
+
+//function
+function greet(name, lastName)
+
+{
+    console.log('Hello' + ' ' + name + ' ' + lastName)
+}
+greet('Ashok', 'Bohara' );
+greet('bohara','ashok')
+
+
+function square(number)
+{
+    return number*number;
+}
+console.log(square(12));
+
